f4cb03db13fcf8a15695a44d7a90cc1646823835
n1mah <nima.heidari.n@gmail.com>
2023-05-30 21:44:57 +0330

    Install Tailwind & Flowbite & Tests on First Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,12 @@ module.exports = {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-<<<<<<< HEAD
         "./node_modules/flowbite/**/*.js"
-=======
->>>>>>> origin/main
     ],
     theme: {
         extend: {},
     },
-<<<<<<< HEAD
     plugins: [
         require('flowbite/plugin')
     ],
-=======
-    plugins: [],
->>>>>>> origin/main
 }
