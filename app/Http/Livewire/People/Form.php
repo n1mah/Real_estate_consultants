30d4d7ccdb38fb0057ad3b677172f13a5c68d924
n1mah <nima.heidari.n@gmail.com>
2023-06-01 20:08:22 +0330

    Implement Add Form People

--- app/Http/Livewire/People/Form.php
+++ app/Http/Livewire/People/Form.php
@@ -2,10 +2,52 @@
 
 namespace App\Http\Livewire\People;
 
+use App\Models\Person;
 use Livewire\Component;
 
 class Form extends Component
 {
+
+    public Person $person;
+
+    public function mount()
+    {
+        $this->person = new Person();
+    }
+    protected $rules = [
+        'person.firstname' => 'required',
+        'person.lastname' => 'required',
+        'person.fathername' => 'required',
+        'person.national_code' => 'required',
+        'person.birth_certificate_number' => 'required',
+        'person.place_of_birth' => 'required',
+        'person.postal_code' => 'required',
+        'person.mobile' => 'required',
+        'person.phone' => 'nullable',
+        'person.address' => 'required',
+        'person.date_of_birth' => 'required',
+    ];
+
+    protected $messages = [
+        'person.firstname.required' => 'نام را وارد کنید',
+        'person.lastname.required' => 'نام خانوادگی  را وارد کنید',
+        'person.fathername.required' => 'نام پدر را وارد کنید',
+        'person.national_code.required' => 'کدملی را وارد کنید',
+        'person.birth_certificate_number.required' => 'شماره شناسنامه را وارد کنید',
+        'person.place_of_birth.required' => 'محل تولد را وارد کنید',
+        'person.postal_code.required' => 'کدپستی را وارد کنید',
+        'person.mobile.required' => 'موبایل را وارد کنید',
+        'person.address.required' => 'آدرس خود را وارد کنید',
+        'person.date_of_birth.required' => 'تاریخ تولد را وارد کنید',
+    ];
+
+    public function create()
+    {
+//        dd(1);
+        $this->validate();
+        $this->person->save();
+        redirect()->route('people');
+    }
     public function render()
     {
         return view('livewire.people.form')
--- resources/views/livewire/people/form.blade.php
+++ resources/views/livewire/people/form.blade.php
@@ -4,52 +4,52 @@
         <hr>
     </div>
     <div class="max-w-3xl px-4 py-2 mx-auto">
-                <form action="#" class=" py-2">
+                <form wire:submit.prevent="create" class="py-2">
                     <div class="grid gap-4 mb-4 sm:grid-cols-3 sm:gap-6 sm:mb-5">
                         <div class="w-full">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام <span class="text-red-600">*</span></label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="نام را وارد کنید ...">
+                            <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام <span class="text-red-600">*</span></label>
+                            <input type="text" name="firstname" id="firstname" wire:model.defer="person.firstname" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="نام را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام خانوادگی<span class="text-red-600">*</span></label>
-                            <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="نام خانوادگی را وارد کنید ...">
+                            <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام خانوادگی<span class="text-red-600">*</span></label>
+                            <input type="text" name="lastname" id="lastname" wire:model.defer="person.lastname" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="نام خانوادگی را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام پدر<span class="text-red-600">*</span></label>
-                            <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="نام پدر را وارد کنید ...">
+                            <label for="fathername" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام پدر<span class="text-red-600">*</span></label>
+                            <input type="text" name="fathername" id="fathername" wire:model.defer="person.fathername" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="نام پدر را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">کدملی<span class="text-red-600">*</span></label>
-                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="کد ملی را وارد کنید ...">
+                            <label for="national_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">کدملی<span class="text-red-600">*</span></label>
+                            <input type="number" name="national_code" id="national_code" wire:model.defer="person.national_code" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="کد ملی را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">شماره شناسنامه<span class="text-red-600">*</span></label>
-                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="0" placeholder="شماره شناسنامه را وارد کنید ...">
+                            <label for="birth_certificate_number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">شماره شناسنامه<span class="text-red-600">*</span></label>
+                            <input type="number" name="birth_certificate_number" id="birth_certificate_number" wire:model.defer="person.birth_certificate_number" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="0" placeholder="شماره شناسنامه را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">محل تولد<span class="text-red-600">*</span></label>
-                            <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="شهر تولد را وارد کنید ...">
+                            <label for="place_of_birth" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">محل تولد<span class="text-red-600">*</span></label>
+                            <input type="text" name="place_of_birth" id="place_of_birth" wire:model.defer="person.place_of_birth" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="شهر تولد را وارد کنید ...">
                         </div>
 
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">کد پستی<span class="text-red-600">*</span></label>
-                            <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="کدپستی را وارد کنید ...">
+                            <label for="postal_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">کد پستی<span class="text-red-600">*</span></label>
+                            <input type="text" name="postal_code" id="postal_code" wire:model.defer="person.postal_code" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="کدپستی را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">موبایل<span class="text-red-600">*</span></label>
-                            <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="موبایل را وارد کنید ...">
+                            <label for="mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">موبایل<span class="text-red-600">*</span></label>
+                            <input type="text" name="mobile" id="mobile" wire:model.defer="person.mobile" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="موبایل را وارد کنید ...">
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">تلفن</label>
-                            <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="تلفن را وارد کنید ...">
+                            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">تلفن</label>
+                            <input type="text" name="phone" id="phone" wire:model.defer="person.phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="تلفن را وارد کنید ...">
                         </div>
                         <div class="sm:col-span-3">
                             <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">آدرس<span class="text-red-600">*</span></label>
-                            <textarea id="address" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="آدرس را وارد کنید ..."></textarea>
+                            <textarea id="address" name="address" rows="4" required wire:model.defer="person.address" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="آدرس را وارد کنید ..."></textarea>
                         </div>
                         <div class="w-full">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">تاریخ تولد<span class="text-red-600">*</span></label>
-                            <input type="text" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="تاریخ تولد را وارد کنید ...">
+                            <label for="date_of_birth" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">تاریخ تولد<span class="text-red-600">*</span></label>
+                            <input type="text" name="date_of_birth" id="date_of_birth" wire:model.defer="person.date_of_birth" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="1990/02/02 10:10:10" placeholder="تاریخ تولد را وارد کنید ...">
                         </div>
 
                     </div>
