728e2cb47d71b97f61b9e82ac56da2d8015f5c81
n1mah <nima.heidari.n@gmail.com>
2023-06-01 23:01:10 +0330

    Implement Validation Add Form People & show error

--- app/Http/Livewire/People/Form.php
+++ app/Http/Livewire/People/Form.php
@@ -15,35 +15,50 @@ class Form extends Component
         $this->person = new Person();
     }
     protected $rules = [
-        'person.firstname' => 'required',
-        'person.lastname' => 'required',
-        'person.fathername' => 'required',
-        'person.national_code' => 'required',
+        'person.firstname' => 'required|min:3|max:30',
+        'person.lastname' => 'required|min:3|max:30',
+        'person.fathername' => 'required|min:3|max:30',
+        'person.national_code' => 'required|digits:10|unique:people,national_code',
         'person.birth_certificate_number' => 'required',
-        'person.place_of_birth' => 'required',
+        'person.place_of_birth' => 'required|max:55',
         'person.postal_code' => 'required',
-        'person.mobile' => 'required',
-        'person.phone' => 'nullable',
-        'person.address' => 'required',
+        'person.mobile' => 'required|integer|digits:11',
+        'person.phone' => 'nullable|integer|min:8|max:13',
+        'person.address' => 'required|min:6',
         'person.date_of_birth' => 'required',
     ];
 
     protected $messages = [
         'person.firstname.required' => 'نام را وارد کنید',
+        'person.firstname.min' => 'نام باید حداقل 3 کارکتر باشد',
+        'person.firstname.max' => 'نام باید حداکثر 30 کارکتر باشد',
         'person.lastname.required' => 'نام خانوادگی  را وارد کنید',
+        'person.lastname.min' => 'نام باید حداقل 3 کارکتر باشد',
+        'person.lastname.max' => 'نام باید حداکثر 30 کارکتر باشد',
         'person.fathername.required' => 'نام پدر را وارد کنید',
+        'person.fathername.min' => 'نام پدر باید حداقل 3 کارکتر باشد',
+        'person.fathername.max' => 'نام پدر باید حداکثر 30 کارکتر باشد',
         'person.national_code.required' => 'کدملی را وارد کنید',
+        'person.national_code.digits' => 'کدملی باید 10 عدد باشد',
+        'person.national_code.numeric' => 'کدملی باید عدد باشد',
+        'person.national_code.unique' => 'مشتری با این کد ملی از قبل در نرم افزار وجود دارد',
         'person.birth_certificate_number.required' => 'شماره شناسنامه را وارد کنید',
         'person.place_of_birth.required' => 'محل تولد را وارد کنید',
+        'person.place_of_birth.max' => 'محل تولد حداکثر باید 55 کارکتر باشد',
         'person.postal_code.required' => 'کدپستی را وارد کنید',
         'person.mobile.required' => 'موبایل را وارد کنید',
+        'person.mobile.numeric' => 'شماره موبایل باید عدد باشد',
+        'person.mobile.digits' => 'شماره موبایل باید 11 رقم باشد',
+        'person.phone.numeric' => 'شماره تلفن باید عدد باشد',
+        'person.phone.min' => 'شماره تلفن نامعتبر می باشد',
+        'person.phone.max' => 'شماره تلفن نامعتبر می باشد',
         'person.address.required' => 'آدرس خود را وارد کنید',
+        'person.address.min' => 'آدرس باید حداقل 6 کارکتر باشد',
         'person.date_of_birth.required' => 'تاریخ تولد را وارد کنید',
     ];
 
     public function create()
     {
-//        dd(1);
         $this->validate();
         $this->person->save();
         redirect()->route('people');
