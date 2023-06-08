cecad37917db4e267a24fece15b6fa4a0bb75ee4
n1mah <nima.heidari.n@gmail.com>
2023-06-08 23:39:12 +0330

    Change Model

--- app/Models/Financial.php
+++ app/Models/Financial.php
@@ -9,7 +9,7 @@ class Financial extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'purchase_price','deposit','payment_type','bank','branch'
+        'contract_of_sale_id','purchase_price','deposit','payment_type','bank','branch'
     ];
     public function contract_of_sale()
     {
