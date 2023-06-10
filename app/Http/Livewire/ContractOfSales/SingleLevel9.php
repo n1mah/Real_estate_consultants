48a2c8d221dc44a540b6df2e21c55f5fbd0c887f
n1mah <nima.heidari.n@gmail.com>
2023-06-10 21:23:12 +0330

    Add & Implement Single Level 9

---
+++ app/Http/Livewire/ContractOfSales/SingleLevel9.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire\ContractOfSales;
+
+use App\Models\ContractOfSale;
+use App\Models\Financial;
+use App\Models\User;
+use Livewire\Component;
+
+class SingleLevel9 extends Component
+{
+    public ContractOfSale $contractOfSale;
+    public $user_id;
+    public $user_manager;
+    public $user_address;
+    public $user_membership_number;
+    public $signature_date;
+    public $signature_hour;
+    public $signature_minutes;
+
+    public function mount(){
+        $this->signature_date=time();
+        $user=auth()->user();
+
+
+//        if ($this->contractOfSale->count()==1){
+
+            $user=User::where('id',$this->contractOfSale->user_id)->first();
+            $this->user_id=$user->id;
+            $this->user_address=$user->address;
+            $this->user_membership_number=$user->membership_number;
+            $this->user_manager=$user->manager;
+
+            $this->signature_date=$this->contractOfSale->signature_date;
+            $this->signature_hour=$this->contractOfSale->signature_hour;
+            $this->signature_minutes=$this->contractOfSale->signature_minutes;
+
+//            $total=(($this->amount_received_each*2)+$this->wage);
+//            $this->total_received=((($total*$this->tax)/100)+$total);
+//        }else{
+//            redirect()->route('sales');
+//        }
+
+    }
+    public function render()
+    {
+        return view('livewire.contract-of-sales.single-level9')
+            ->layout('components.layouts.app');
+    }
+}
