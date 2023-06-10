079fe044883ca457a77fc5ea3d2808d01a6ff9a0
n1mah <nima.heidari.n@gmail.com>
2023-06-11 03:04:11 +0330

    Add DetailsOfRental Model

---
+++ app/Models/DetailsOfRental.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailsOfRental extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'lease_agreement_id','rental_period','rental_period_type','from','until',
+        'penalty_for_non_payment','penalty_for_non_return','delivery_time',
+        'penalty_for_non_delivery','others','damages_for_non_fulfillment_of_obligations',
+        'penalty_for_non_evacuation'
+    ];
+}
---
+++ database/migrations/2023_06_10_231442_create_details_of_rentals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('details_of_rentals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("lease_agreement_id"); //کد قرارداد اجاره
+            $table->foreign("lease_agreement_id")->references("id")->on("lease_agreements")->onUpdate("cascade")->onDelete("cascade");
+            $table->string('rental_period'); //مدت زمان اجاره
+            $table->string('rental_period_type'); //نوع مورد اجاره //سال/ماه
+            $table->timestamp('from')->nullable(); // اجاره از تاریخ
+            $table->timestamp('until')->nullable(); // اجاره تا تاریخ
+            $table->decimal("penalty_for_non_payment",30,0)->nullable();  //خسارت عدم وصول مبلغ (چک)
+            $table->decimal("penalty_for_non_return",30,0)->nullable();  //خسارت عدم استرداد مبلغ ( به صورت روزانه )
+            $table->timestamp('delivery_time')->nullable(); // زمان تحویل
+            $table->decimal("penalty_for_non_delivery",30,0)->nullable();  //خسارت عدم تحویل (مورداجاره)
+            $table->string('others'); //غیر
+            $table->decimal("damages_for_non_fulfillment_of_obligations",30,0)->nullable();  //خسارت عدم انجام تعهدات مستاجر
+            $table->decimal("penalty_for_non_evacuation",30,0)->nullable();  //جریمه عدم تسلیم (مورداجاره) توسط مستاجر ( به صورت روزانه )
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('details_of_rentals');
+    }
+};
