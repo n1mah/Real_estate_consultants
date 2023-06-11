2fee254a6bbe2e0c126402bec8f7f1d8ac000af5
n1mah <nima.heidari.n@gmail.com>
2023-06-11 11:42:55 +0330

    Add & Implement SingleLevel1 LeaseAgreements

---
+++ app/Http/Livewire/LeaseAgreements/SingleLevel1.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Livewire\LeaseAgreements;
+
+use App\Models\LeaseAgreement;
+use App\Models\Person;
+use Livewire\Component;
+
+class SingleLevel1 extends Component
+{
+    public LeaseAgreement $leaseAgreement;
+
+    public function getPerson($id)
+    {
+        return Person::find($id);
+    }
+
+    public function mount()
+    {
+        if ($this->leaseAgreement->level>1){
+            //
+        }else{
+            redirect()->route('rents');
+
+        }
+
+    }
+
+    public function render()
+    {
+        $fileNumber=$this->leaseAgreement->file_number;
+        $people_selectedTenant=$this->leaseAgreement->peopleTenants()->get();
+        $people_selectedLessor=$this->leaseAgreement->peopleLessors()->get();
+        $people_selectedTenant=$people_selectedTenant->map(function ($person_selectedTenant) {
+            $data= collect($person_selectedTenant)->only(['id', 'firstname', 'lastname','pivot']);
+            $data['lawyer']= $this->getPerson($data['pivot']['lawyer_id']);
+            return $data;
+        });
+        $people_selectedLessor=$people_selectedLessor->map(function ($person_selectedLessor) {
+            $data= collect($person_selectedLessor)->only(['id', 'firstname', 'lastname','pivot']);
+            $data['lawyer']= $this->getPerson($data['pivot']['lawyer_id']);
+            return $data;
+        });
+        return view('livewire.lease-agreements.single-level1',[
+            'fileNumber'=>$fileNumber,
+            'people_selectedTenant'=>$people_selectedTenant,
+            'people_selectedLessor'=>$people_selectedLessor,
+        ])
+            ->layout('components.layouts.app');
+
+    }
+}
---
+++ resources/views/livewire/lease-agreements/single-level1.blade.php
@@ -0,0 +1,61 @@
+<section class="w-full mx-auto bg-gray-50 dark:bg-gray-900 p-3 sm:p-5" style="min-height: 100vh">
+    <div class="w-full-65 px-4 py-2 mx-auto flex-col flex items-center justify-center">
+        <h2 class="font-medium p-3 text-gray-400">مشاهده ماده 1 - طرفین قرارداد</h2>
+        <hr class="p-2 w-full">
+        <p class="text-center text-gray-600"> شماره پرونده <span class="text-warning-700"> {{$fileNumber}}</span></p>
+        <div class="w-full flex justify-between items-start flex-wrap">
+            <div class="w-full-45 flex flex-row flex-wrap justify-center">
+                @if($people_selectedTenant)
+                    <hr>
+                    <p class="text-center text-success-700 my-2">
+                        @if(count($people_selectedTenant)>1) مستاجران @else مستاجر @endif
+                    </p>
+                    <div class="w-full flex flex-col flex-wrap items-center justify-start">
+                        @foreach($people_selectedTenant as $person_selected)
+                            <div  class="w-full flex flex-col items-center justify-center m-1 px-5 py-2.5 text-sm font-medium text-center text-white bg-green-600 rounded-lg focus:ring-4 focus:outline-none ">
+                                {{$person_selected['firstname']}} {{$person_selected['lastname']}}
+                                <div class="mt-4 w-full">
+                                    @if($person_selected['lawyer']!=null)
+                                        <div class="flex flex-col items-center justify-center w-full  text-xs font-medium text-yellow-400 bg-yellow-900 rounded-[16px] px-3 py-2">
+                                            وکیل دارد:
+                                            <div class="py-1"></div>
+                                            <div>{{$person_selected['lawyer']['firstname']}} {{$person_selected['lawyer']['lastname']}}</div>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+            <div class="w-full-45 flex flex-row flex-wrap justify-center">
+                @if($people_selectedLessor)
+                    <hr>
+                    <p class=" text-center my-2 text-red-700">
+                        @if(count($people_selectedLessor)>1) موجران @else موجر @endif
+                    </p>
+                    <div class="w-full flex flex-col flex-wrap items-center justify-start">
+                        @foreach($people_selectedLessor as $person_selected)
+                            <div  class="w-full flex flex-col items-center justify-center m-1 px-5 py-2.5 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:outline-none ">
+                                {{$person_selected['firstname']}} {{$person_selected['lastname']}}
+                                <div class="mt-4 w-full">
+                                    @if($person_selected['lawyer']!=null)
+                                        <div class="flex flex-col items-center justify-center w-full  text-xs font-medium text-yellow-400 bg-yellow-900 rounded-[16px] px-3 py-2">
+                                            وکیل دارد:
+                                            <div class="py-1"></div>
+                                            <div>{{$person_selected['lawyer']['firstname']}} {{$person_selected['lawyer']['lastname']}}</div>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+        </div>
+        <br>
+        <hr class="w-full-45 p-2">
+        <br>
+        <a href="{{route('rents')}}" class="p-4 bg-primary-600 text-white rounded-[10px]"> بازگشت به صفحه اجاره نامه ها</a>
+    </div>
+</section>
