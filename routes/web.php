b19b757a247c48ee974f3eee0667bb79e018c954
n1mah <nima.heidari.n@gmail.com>
2023-06-11 11:43:05 +0330

    Use SingleLevel1 LeaseAgreements

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ use App\Http\Livewire\ContractOfSales\Level10 as Sale_Add_Level10;
 
 use App\Http\Livewire\LeaseAgreements\Index as Rent_Index;
 use App\Http\Livewire\LeaseAgreements\Add as Rent_Add;
+use App\Http\Livewire\LeaseAgreements\SingleLevel1 as Single1;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -78,5 +79,6 @@ Route::get('/sales/level10/{contractOfSale}', Sale_Add_Level10::class)->name("sa
 
 Route::get('/rents', Rent_Index::class)->name("rents");
 Route::get('/rents/level1', Rent_Add::class)->name("rent.add");
+Route::get('/rents/level1/single/{leaseAgreement}', Single1::class)->name("rent.level1.single");
 
 
