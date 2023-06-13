cde0d0cf223861d86f9e901f813c8252e080205c
n1mah <nima.heidari.n@gmail.com>
2023-06-13 12:50:12 +0330

    Empty Data Contract

--- database/migrations/2023_05_31_104008_create_contract_of_sales_table.php
+++ database/migrations/2023_05_31_104008_create_contract_of_sales_table.php
@@ -20,22 +20,22 @@ return new class extends Migration
             $table->timestamp("confirm_date")->nullable();
             $table->timestamps();
         });
-        $data=[
-            ["133456-122","1685531870",'a'],
-            ["133456-123","1590923870",'b'],
-            ["133456-124","1433071070",'c'],
-        ];
-        for ($i = 0; $i < count($data); $i++) {
-            DB::table('contract_of_sales')->insert(
-                [
-                    'level' => 0,
-                    'file_number' => $data[$i][0],
-                    'confirm_date' => Carbon::createFromTimestamp($data[$i][1])->format("Y/m/d H:i:s"),
-                    'created_at' => Carbon::createFromTimestamp(time())->format("Y/m/d H:i:s"),
-                    'updated_at' => Carbon::createFromTimestamp(time())->format("Y/m/d H:i:s"),
-                ]
-            );
-        }
+//        $data=[
+//            ["133456-122","1685531870",'a'],
+//            ["133456-123","1590923870",'b'],
+//            ["133456-124","1433071070",'c'],
+//        ];
+//        for ($i = 0; $i < count($data); $i++) {
+//            DB::table('contract_of_sales')->insert(
+//                [
+//                    'level' => 0,
+//                    'file_number' => $data[$i][0],
+//                    'confirm_date' => Carbon::createFromTimestamp($data[$i][1])->format("Y/m/d H:i:s"),
+//                    'created_at' => Carbon::createFromTimestamp(time())->format("Y/m/d H:i:s"),
+//                    'updated_at' => Carbon::createFromTimestamp(time())->format("Y/m/d H:i:s"),
+//                ]
+//            );
+//        }
     }
 
     /**
